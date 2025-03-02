ffbb225b44973f50279c0976cf73cf093bb5f25b
Jovan <167636968+JOVICA11@users.noreply.github.com>
2025-03-02 13:31:26 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("JavaScript Loaded!");
 
+    
+    const menu = document.querySelector(".menu");
+    const menuButton = document.createElement("button");
+    menuButton.innerText = "☰ Menu";
+    menuButton.classList.add("menu-toggle");
+    document.querySelector("header").prepend(menuButton);
+
+    menuButton.addEventListener("click", function () {
+        menu.classList.toggle("active");
+    });
+
+    
+    const exploreButton = document.querySelector(".button");
+    if (exploreButton) {
+        exploreButton.addEventListener("click", function () {
+            alert("Explore our latest collection!");
+        });
+    }
+
+    const products = document.querySelectorAll(".card");
+    products.forEach((product) => {
+        const addButton = document.createElement("button");
+        addButton.innerText = "Add to Cart";
+        addButton.classList.add("add-to-cart");
+        product.appendChild(addButton);
+
+        addButton.addEventListener("click", function () {
+            const itemName = product.querySelector(".text-card").innerText;
+            alert(`${itemName} added to cart!`);
+        });
+    });
+});
